b016df6a305dbcd29ccf6d79f2443ae104a9eebe
tyamada3 <tyamada3@OVDIMC10-313V.coast.cccd.adx>
2018-05-08 20:16:25 -0700

    Test

--- Capstone/src/edu/orangecoastcollege/cs272/capstone/model/DBModel.java
+++ Capstone/src/edu/orangecoastcollege/cs272/capstone/model/DBModel.java
@@ -10,45 +10,46 @@ import java.util.ArrayList;
 
 public class DBModel {
 
+
 	private String mDBName;
 	private String mTableName;
 	private String[] mFieldNames;
 	private String[] mFieldTypes;
-	
+
 	public DBModel(String mDBName, String mTableName, String[] mFieldNames, String[] mFieldTypes) throws SQLException {
 		super();
 		this.mDBName = mDBName;
 		this.mTableName = mTableName;
 		this.mFieldNames = mFieldNames;
 		this.mFieldTypes = mFieldTypes;
-		
+
 		if (mFieldNames == null || mFieldTypes == null || mFieldNames.length == 0 || mFieldNames.length != mFieldTypes.length)
 			throw new SQLException("Database field names and types must exist and have the same number of elements.");
-		
+
 		createTable();
-		
+
 	}
 
 	private void createTable() throws SQLException {
-		
+
 		try (Connection connection = connectToDB();
 				Statement stmt = connection.createStatement())
 		{
 		StringBuilder createSQL = new StringBuilder("CREATE TABLE IF NOT EXISTS ");
-		
+
 		createSQL.append(mTableName).append("(");
-		
+
 		for (int i = 0; i < mFieldNames.length; i++)
 			createSQL.append(mFieldNames[i]).append(" ").append(mFieldTypes[i])
 					.append((i < mFieldNames.length - 1) ? "," : ")");
-		
+
 		stmt.executeUpdate(createSQL.toString());
 		}
-		
+
 	}
-	
+
 	public ArrayList<ArrayList<String>> getAllRecords() throws SQLException {
-		
+
 		try (Connection connection = connectToDB();
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery("SELECT * FROM " + mTableName);)
@@ -64,15 +65,15 @@ public class DBModel {
 			return resultsList;
 		}
 	}
-	
+
 	public ArrayList<ArrayList<String>> getRecord(String key) throws SQLException {
 		try (Connection connection = connectToDB();
 				Statement stmt = connection.createStatement();
 					ResultSet rs = stmt.executeQuery("SELECT * FROM " + mTableName + " WHERE " + mFieldNames[0] + " = " + key);)
-			{		
+			{
 				ResultSetMetaData rsData = rs.getMetaData();
 				ArrayList<ArrayList<String>> resultsList = new ArrayList<>();
-				
+
 				int cols = rsData.getColumnCount();
 				while (rs.next())
 				{
@@ -84,7 +85,7 @@ public class DBModel {
 				return resultsList;
 			}
 	}
-	
+
 	public int createRecord(String[] fields, String[] values) throws SQLException {
 
 		if (fields == null || values == null || fields.length == 0 || fields.length != values.length)
@@ -103,18 +104,18 @@ public class DBModel {
 		return stmt.getGeneratedKeys().getInt(1);
 		}
 	}
-	
+
 	public boolean updateRecord(String key, String[] fields, String[] values) throws SQLException {
-		
+
 		if (fields == null || values == null || fields.length == 0 || fields.length != values.length)
 			return false;
-		
+
 		try (Connection connection = connectToDB();
 				Statement stmt = connection.createStatement())
 		{
 		StringBuilder updateSQL = new StringBuilder("UPDATE ");
 		updateSQL.append(mTableName).append(" SET ");
-		
+
 		for (int i = 0; i < fields.length; i++)
 			updateSQL.append(fields[i]).append("=").append(convertToSQLText(fields[i], values[i]))
 					.append((i < fields.length - 1) ? ", " : " ");
@@ -124,9 +125,9 @@ public class DBModel {
 		return true;
 		}
 	}
-	
+
 	private String convertToSQLText(String field, String value) {
-		
+
 		for (int i = 0; i < mFieldNames.length; i++) {
 			if (mFieldNames[i].equals(field)) {
 				if (mFieldTypes[i].toUpperCase().startsWith("TEXT"))
@@ -138,17 +139,17 @@ public class DBModel {
 	}
 
 	private Connection connectToDB() throws SQLException {
-		
+
 		try {
 			Class.forName("org.sqlite.JDBC");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		Connection connection = DriverManager.getConnection("jdbc:sqlite:" + mDBName);
 		return connection;
 	}
-	
-	
+
+
 
 }
